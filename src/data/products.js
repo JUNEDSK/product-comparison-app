3fb6f0510e5f943c544714b115f3918fa59d46e1
JUNEDSK <js.juned@gmail.com>
2025-07-12 15:29:56 +0530

    Add structured product data with image URLs and detailed features

---
+++ src/data/products.js
@@ -0,0 +1,114 @@
+export const products = [
+  {
+    id: '1',
+    name: 'iPhone 15 Pro',
+    brand: 'Apple',
+    image: 'https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=400',
+    price: 999,
+    features: {
+      screenSize: '6.1"',
+      battery: '3274 mAh',
+      camera: '48MP Triple',
+      storage: '128GB',
+      processor: 'A17 Pro'
+    }
+  },
+  {
+    id: '2',
+    name: 'Galaxy S24 Ultra',
+    brand: 'Samsung',
+    image: 'https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=400',
+    price: 1199,
+    features: {
+      screenSize: '6.8"',
+      battery: '5000 mAh',
+      camera: '200MP Quad',
+      storage: '256GB',
+      processor: 'Snapdragon 8 Gen 3'
+    }
+  },
+  {
+    id: '3',
+    name: 'Pixel 8 Pro',
+    brand: 'Google',
+    image: 'https://images.pexels.com/photos/1294943/pexels-photo-1294943.jpeg?auto=compress&cs=tinysrgb&w=400',
+    price: 899,
+    features: {
+      screenSize: '6.7"',
+      battery: '5050 mAh',
+      camera: '50MP Triple',
+      storage: '128GB',
+      processor: 'Tensor G3'
+    }
+  },
+  {
+    id: '4',
+    name: 'iPhone 14',
+    brand: 'Apple',
+    image: 'https://images.pexels.com/photos/14999419/pexels-photo-14999419.jpeg?auto=compress&cs=tinysrgb&w=400',
+    price: 799,
+    features: {
+      screenSize: '6.1"',
+      battery: '3279 mAh',
+      camera: '12MP Dual',
+      storage: '128GB',
+      processor: 'A15 Bionic'
+    }
+  },
+  {
+    id: '5',
+    name: 'OnePlus 12',
+    brand: 'OnePlus',
+    image: 'https://images.pexels.com/photos/1038628/pexels-photo-1038628.jpeg?auto=compress&cs=tinysrgb&w=400',
+    price: 799,
+    features: {
+      screenSize: '6.82"',
+      battery: '5400 mAh',
+      camera: '50MP Triple',
+      storage: '256GB',
+      processor: 'Snapdragon 8 Gen 3'
+    }
+  },
+  {
+    id: '6',
+    name: 'Galaxy S23',
+    brand: 'Samsung',
+    image: 'https://images.pexels.com/photos/17663316/pexels-photo-17663316.jpeg?auto=compress&cs=tinysrgb&w=400',
+    price: 699,
+    features: {
+      screenSize: '6.1"',
+      battery: '3900 mAh',
+      camera: '50MP Triple',
+      storage: '128GB',
+      processor: 'Snapdragon 8 Gen 2'
+    }
+  },
+  {
+    id: '7',
+    name: 'Pixel 7a',
+    brand: 'Google',
+    image: 'https://images.pexels.com/photos/19670073/pexels-photo-19670073.jpeg?auto=compress&cs=tinysrgb&w=400',
+    price: 499,
+    features: {
+      screenSize: '6.1"',
+      battery: '4385 mAh',
+      camera: '64MP Dual',
+      storage: '128GB',
+      processor: 'Tensor G2'
+    }
+  },
+  {
+    id: '8',
+    name: 'iPhone 13 Mini',
+    brand: 'Apple',
+    image: 'https://images.pexels.com/photos/6010/smartphone-desk-smartphone-technology.jpg?auto=compress&cs=tinysrgb&w=400',
+    price: 629,
+    features: {
+      screenSize: '5.4"',
+      battery: '2438 mAh',
+      camera: '12MP Dual',
+      storage: '128GB',
+      processor: 'A15 Bionic'
+    }
+  }
+];
